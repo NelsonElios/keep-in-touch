9017308dc65b420244b89928706c7bb6c1fda5c4
Nelson ALATE <nalate@asi.fr>
2018-05-11 16:23:54 +0200

    first commit and push

--- src/pages/form/form.ts
+++ src/pages/form/form.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {BarcodeScanResult} from "@ionic-native/barcode-scanner";
 
-/**
+/***
  * Generated class for the FormPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
